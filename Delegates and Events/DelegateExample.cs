e34e08921897c9c004e96bb29b72de1357816a3d
garethS-debug <76433148+garethS-debug@users.noreply.github.com>
2021-10-12 18:03:53 +0100

    Add files via upload

---
+++ Delegates and Events/DelegateExample.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelegateExample : MonoBehaviour {
+
+
+	//executing methods in other scripts (Multicasting) 
+	//----WARNING -------
+	//resource leaks can happen with delegates 
+
+	
+	//DELEGATE
+		//delegate with an int return type
+		public delegate int MyTestDelegate(float valueToBeRounded);
+
+		//creating an instance of the delegate. Points to the above delegate
+		private MyTestDelegate newtestDelegate;
+
+
+	//EVENT
+		//base upon which event is built (without it the event cant be built) 
+		public delegate void UpdateInfoDelegate();
+		//Accesser - return type is the delegate
+		public static event UpdateInfoDelegate UpdateInfoEvent;
+		internal static float timer = 0.0f;
+		public float interval = 4.0f;
+		//Another event example
+		public static event MyTestDelegate ValueRoundingEvent;
+
+	private void OnEnabled()
+    {
+		//In order for the delegate to run the method, it needs to be subscribed. 
+		//Delegate Ref		//Method name no ()
+		newtestDelegate = RoundTime;
+		newtestDelegate = RoundMyAge;
+
+		//both functions will be run 
+
+
+    }
+
+	//Same return type needed
+	public int RoundTime(float valueRounded)
+    {
+		return Mathf.RoundToInt(valueRounded);
+    }
+
+	//Example of another method 
+	public int RoundMyAge(float ageValue)
+    {
+		return Mathf.RoundToInt(ageValue);
+	}
+
+	private void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+			//Calling Delegates
+			Debug.Log("Rounded time value: " + newtestDelegate(Time.time));
+			Debug.Log("Rounded age value: " + newtestDelegate(Random.Range(0,100)));
+			
+			//Calling event
+			UpdateInfoEvent();
+			ValueRoundingEvent(interval);
+
+		}
+    }
+
+
+}
---
+++ Delegates and Events/OutSubscriberClass.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutSubscriberClass : MonoBehaviour {
+    
+
+
+
+    private void ReflectMessage()
+    {
+        Debug.Log("This is from the other class");
+        
+    }
+
+
+    //Destroy this script
+    private void DestroyComponent()
+    {
+        Destroy(this, 4.0f);
+    }
+
+
+    private void Start()
+    {
+        //You cannot just assign a function to the event. It should be added to invocation list or removed, instead of just being 'called'. 
+        // e.g. DelegateExample.UpdateInfoEvent = ReflectMessage; wont work due to protection
+        DelegateExample.UpdateInfoEvent += ReflectMessage;
+        //Antoher Expample
+        DelegateExample.ValueRoundingEvent += DelegateExamplesOnValueRoundingEvent;
+       
+        
+        DestroyComponent();
+    }
+
+    private int DelegateExamplesOnValueRoundingEvent (float valuetoberounded)
+    {
+        return Mathf.RoundToInt(valuetoberounded);
+    }
+
+    //On destroy it removes the delegate from the invocation list 
+    private void OnDestroy()
+    {
+        DelegateExample.UpdateInfoEvent -= ReflectMessage;
+        DelegateExample.ValueRoundingEvent -= DelegateExamplesOnValueRoundingEvent;
+    }
+}
